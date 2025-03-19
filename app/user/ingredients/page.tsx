85c23e6dcc389ca9de90792e5405e6c54944bfef
tetsugo-mac <tetsugou2002@gmail.com>
2025-03-19 11:33:24 +0900

    食材追加モーダルのボタンとスタイルを改善

--- app/user/ingredients/page.tsx
+++ app/user/ingredients/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { useState } from 'react';
-import { PageContainer } from '@/components/atom/pageContainer';
+import { Plus } from 'lucide-react';
+
 import { Button } from '@/components/ui/button';
 import { AddNewIngredientModal } from '@/components/pages/modal/addNewIngredientModal';
 
@@ -11,8 +12,15 @@ const IngredientsPage = () => {
     <div className='flex flex-col pt-4 h-full w-4/5 mx-auto'>
       <div className='flex justify-between'>
         <h1 className='text-2xl font-bold w-1/3 pl-4'>食材管理</h1>
-        <span className='pr-4'>
-          <Button onClick={() => setIsOpen(true)}>食材を追加</Button>
+        <span className='pr-4 '>
+          <Button
+            onClick={() => setIsOpen(true)}
+            className=' hover:cursor-pointer bg-blue-400 hover:bg-blue-200 text-white size-12 w-18'
+          >
+            <div className='flex items-center gap-1'>
+              <Plus /> ADD
+            </div>
+          </Button>
         </span>
       </div>
       <div className='border-t mx-3 my-2'></div>
